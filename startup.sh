75c820ebd1e5b9578c318994f5d63be0dda5078d
miskinole2002 <144457278+miskinole2002@users.noreply.github.com>
2025-07-29 07:01:59 -0400

    update

--- startup.sh
+++ startup.sh
@@ -1,1 +1,4 @@
-uvicorn Backend.app:app --host 0.0.0.0 --port 8002 --workers 1 --reload 
+# uvicorn Backend.app:app --host 0.0.0.0 --port 8002 --workers 1 --reload 
+#!/bin/bash
+# Utilise le Python de ton environnement directement pour lancer uvicorn
+./env/Scripts/python -m uvicorn Backend.app:app --reload
