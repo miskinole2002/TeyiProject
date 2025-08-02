10f47abc033e028ba8b64d5904351e46fbb8ced3
miskinole2002 <144457278+miskinole2002@users.noreply.github.com>
2025-08-02 06:28:04 -0400

    add++

--- Backend/app.py
+++ Backend/app.py
@@ -22,6 +22,19 @@ static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"static"))
 app.mount("/static",StaticFiles(directory=static_dir), name="static")
 
 
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://sadeufotsing2002:brO3ZTgP5lNpvQqu@cluster0.xtie1iq.mongodb.net/Teyi?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+db=client['teyi']
+user_collection=client['users']
+
+
+
+
 @app.get("/")
 async def home_page(request:Request):
 
@@ -29,8 +42,37 @@ async def home_page(request:Request):
    
 
         return templates.TemplateResponse("home.html",{"request":request })
-   
 
+@app.get('/register')
+async def signup(request:Request):
+
+        return templates. TemplateResponse('signup.html',{"request":request })
+
+@app.post('/register')
+async def signup(request:Request, nom:str=Form(...), email:str=Form(...), phone:str=Form(...),mdp:str=Form(...)):
+
+        user={
+                'nom':nom,
+                'email':email,
+                'phone':phone,
+                'mdp':mdp,
+                'role':1
+       }
+        x=user_collection.insert_one(user)
+        print(x)
+
+        return RedirectResponse(url="/login",status_code=302)
+
+@app.get('/login')
+async def log(request:Request):
+
+        return templates. TemplateResponse('dashboard.html',{"request":request })
+
+
+@app.get('/add')
+async def addproduct(request:Request):
+
+        return templates. TemplateResponse('dashboard.html',{"request":request })
 
 
 
